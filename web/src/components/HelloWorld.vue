d66bb7dbedc07717260bf014b0ffed1324e50404
cheng <1504468447@qq.com>
2018-08-16 14:22:16 +0800

    基本搭建

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped type="text/css">
 h1, h2 {
   font-weight: normal;
 }
