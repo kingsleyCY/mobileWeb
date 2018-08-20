cc466a74876c08cf9604cfae9c9a31aa7fabda25
cckings <1504468447@qq.com>
2018-08-21 00:04:55 +0800

    123

--- web/src/view/headers/index.vue
+++ web/src/view/headers/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="header-model">
     <div class="container">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-menu hidden-sm-and-up"></i>
       <img src="../../assets/images/logo.jpg" class="left-avator"/>
-      <div class="right-tab hidden-xs-only">
-        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+      <div class="right-tab">
+        <el-menu :default-active="activeIndex"
+                 class="el-menu-demo" :mode="isPc?'vertical':'horizontal'"
+                 :collapse="!isPc">
           <el-menu-item index="1">朝花夕拾</el-menu-item>
           <el-menu-item index="2">四季豆花</el-menu-item>
           <el-menu-item index="3">前端日记</el-menu-item>
@@ -17,37 +19,44 @@
 </template>
 
 <script>
-export default {
-  name: 'header-model',
-  data () {
-    return {
-      activeIndex: '4',
+  export default {
+    name: 'header-model',
+    data() {
+      return {
+        activeIndex: '4',
+        menuMode: 'horizontal',
+        isPc: true
+      }
+    },
+    created() {
+      if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+        this.isPc = false
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-  .header-model{
+<style lang="scss" scoped type="text/scss">
+  .header-model {
     height: 60px;
     background-color: white;
-    .container{
+    .container {
       display: flex;
       justify-content: space-between;
       align-items: center;
       height: 100%;
-      .left-avator{
+      .left-avator {
         width: 45px;
         height: 45px;
         border-radius: 45px;
         display: inline-block;
       }
-      .right-tab{
+      .right-tab {
         height: 100%;
-        .el-menu{
+        .el-menu {
           height: 100%;
-          li{
+          li {
             height: 100%;
             line-height: 60px;
             font-size: 14px;
@@ -55,6 +64,11 @@ export default {
           }
         }
       }
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .container {
+      padding: 0 15px;
     }
   }
 </style>
