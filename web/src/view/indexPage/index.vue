2c1bb8f263cd36a73636f730b00c605e6c784d39
cheng <150468447@qq.com>
2018-09-19 13:53:05 +0800

    请求数据添加

--- web/src/view/indexPage/index.vue
+++ web/src/view/indexPage/index.vue
@@ -114,7 +114,7 @@ export default {
   },
   created() {
     let that = this
-    this.$http.get("https://lionynn.cn/api/article/all").then(function (res) {
+    this.$http.get("http://lionynn.cn/api/article/all").then(function (res) {
       console.log(res);
       that.dynamicList = res.data
     })
