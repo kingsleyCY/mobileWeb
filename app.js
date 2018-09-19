cc624ea2f2db61a0e68ecc2ef8749ab3a7b101b9
cheng <150468447@qq.com>
2018-09-19 14:40:17 +0800

    请求数据添加

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const cors = require('koa-cors');
 
 var app = express();
-app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
