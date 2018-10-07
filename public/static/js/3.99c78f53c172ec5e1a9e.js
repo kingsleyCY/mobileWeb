1da37b853139f7cc691516f307e114a8ef78f345
Lion <1504468447@qq.com>
2018-10-07 11:32:58 +0800

    tag打包

--- public/static/js/3.99c78f53c172ec5e1a9e.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{GHKu:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var i={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{staticStyle:{"text-align":"center","line-height":"300%","font-weight":"bold","font-size":"80px"}},[this._v("\n  coming soon ~~\n")])},staticRenderFns:[]};var a=n("vSla")({name:"some-note",data:function(){return{}}},i,!1,function(e){n("RG+3")},"data-v-5fdf2cda",null);t.default=a.exports},"RG+3":function(e,t){}});
-//# sourceMappingURL=3.99c78f53c172ec5e1a9e.js.map
