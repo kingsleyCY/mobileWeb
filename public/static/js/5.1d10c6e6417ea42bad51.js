c129e6b8fcc4158c0dceaae8c4cb04af98db2e65
cheng <150468447@qq.com>
2018-10-09 13:46:49 +0800

    增加IOCN，修改文本

--- public/static/js/5.1d10c6e6417ea42bad51.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{"1WwP":function(t,e){},Fp9I:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var i={render:function(){var t=this.$createElement;return(this._self._c||t)("div",{staticStyle:{"text-align":"center","line-height":"300%","font-weight":"bold","font-size":"80px"}},[this._v("\n  coming soon ~~\n")])},staticRenderFns:[]};var r=n("vSla")({name:"for-ours",data:function(){return{}}},i,!1,function(t){n("1WwP")},"data-v-0d5d6b7c",null);e.default=r.exports}});
-//# sourceMappingURL=5.1d10c6e6417ea42bad51.js.map
