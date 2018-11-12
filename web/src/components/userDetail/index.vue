81f30788521472ad3f52853ff2038e264e4045cf
cheng <150468447@qq.com>
2018-11-12 18:09:50 +0800

    修改个人详情样式

--- web/src/components/userDetail/index.vue
+++ web/src/components/userDetail/index.vue
@@ -1,24 +1,38 @@
 <template>
-  <div class="">
-    <div class="user-detail-model">
-      <span class="el-icon-close"></span>
+  <div class="user-detail-background" :style="{'display':(userModel?'block':'none')}">
+    <div class="user-detail-model" ref="detailModel">
+      <span class="el-icon-close" @click="closeDetailModel"></span>
+      <div content="content"></div>
     </div>
   </div>
 </template>
 
 <script>
-  import { mapState } from "vuex"
+  import { mapState, mapMutations } from "vuex"
 
   export default {
     name: "user-detail",
     data() {
       return {
-
+        detailModel: null
       }
     },
+    methods: {
+      closeDetailModel() {
+        this.changeUserModel(false)
+      },
+      ...mapMutations(['changeUserModel'])
+    },
     watch: {
-      userModel(oldV, newV) {
-        console.log(newV);
+      userModel(newV, oldV) {
+        this.$nextTick(() => {
+          this.detailModel = this.$refs.detailModel
+          if (newV) {
+            this.detailModel.style.transform = 'translateX(0)'
+          } else {
+            this.detailModel.style.transform = 'translateX(-300px)'
+          }
+        })
       }
     },
     computed: {
@@ -30,17 +44,35 @@
 </script>
 
 <style lang="scss" scoped type="text/scss">
-  .user-detail-model {
+  .user-detail-background {
+    display: block;
     position: fixed;
     top: 0;
     right: 0;
-    width: 300px;
+    width: 100%;
     height: 100%;
     z-index: 100;
-    >.el-icon-close {
+    background-color: rgba(241, 241, 241, 0.1);
+    .user-detail-model {
       position: absolute;
       top: 0;
-      left: -10px;
+      right: 0;
+      width: 300px;
+      height: 100%;
+      z-index: 101;
+      background-color: white;
+      transform: translateX(-300px);
+      transition: transform 1s;
+      > .el-icon-close {
+        position: absolute;
+        top: 0;
+        left: -20px;
+        font-size: 20px;
+        cursor: pointer;
+      }
     }
+  }
+  @keyframes moveAnimation {
+
   }
 </style>
