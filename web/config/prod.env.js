319a7fa8b3e58f3aef9b7ba659bd7a38a55dbd4b
cckings <1504468447@qq.com>
2018-11-20 00:00:43 +0800

    修改判断

--- web/config/prod.env.js
+++ web/config/prod.env.js
@@ -4,11 +4,11 @@ console.log(process.env.npm_lifecycle_event);
 var BASE_API, /* 请求数据URL */
   STATICS_API, /* 静态资源URL */
   BASE_URL; /* 页面访问路径 */
-if (process.env.npm_lifecycle_event == 'cdev') { /*dev*/
+if (process.env.npm_lifecycle_event == 'cbuild') { /*dev*/
   BASE_API = '"https://dev.lionynn.cn"'
   STATICS_API = '"https://dev.lionynn.cn"'
   BASE_URL = 'http://dev.lioncc.cn'
-} else if (process.env.npm_lifecycle_event == 'dev') { /*master*/
+} else if (process.env.npm_lifecycle_event == 'build') { /*master*/
   BASE_API = '"https://lionynn.cn"'
   STATICS_API = '"https://lionynn.cn"'
   BASE_URL = 'http://lioncc.cn'
