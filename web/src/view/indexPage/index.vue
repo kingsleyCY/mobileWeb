dbb0a8e6f498d29896d687c4a0890637e3bc6ccb
cheng <150468447@qq.com>
2018-11-22 13:39:57 +0800

    增加二维码获取

--- web/src/view/indexPage/index.vue
+++ web/src/view/indexPage/index.vue
@@ -51,8 +51,9 @@
                 <p>博客以后会在每两周的星期天晚上12:00更新迭代，希望大家多多支持。</p>
                 <p>后续会加入更多的功能，例如基本的登录注册、权限模块，如果经费及精力条件允许下会尝试的接入一些第三方功能。</p>
                 <p class="update-time">下次更新：{{update_time}}</p>
-                <!--<el-button size="small" @click="getcode">获取二维码</el-button>
-                <img v-if="img" :src="img" style="display: block;width: 100%;height: auto">-->
+                <el-button size="small" @click="getcode">获取二维码</el-button>
+                <img v-if="wx_img" :src="wx_img"
+                     style="display: block;width: 100%;height: auto">
               </div>
             </el-card>
             <!--社交-->
@@ -121,11 +122,10 @@
           type: [],
           label: []
         },
-        img: ''
+        wx_img: ''
       }
     },
     mounted() {
-      console.log(1);
       this.testArticle()
       this.getConfiguration()
     },
@@ -135,9 +135,8 @@
           username: this.username
         }
         this.$store.dispatch('getAssesionToken', param).then(res => {
-          console.log(res);
           if(res.code == 1) {
-            this.img = 'http://localhost:8804/' + res.date
+            this.wx_img = res.date + '?' + Math.random()
           }
         })
       },
