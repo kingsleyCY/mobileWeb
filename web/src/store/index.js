226893506de0ed81d9a5a8524c0764460c2508a8
cheng <1504468447@qq.com>
2019-01-04 14:50:51 +0800

    优化代码

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-//import Vuex from 'vuex'
+// import Vue from 'vue'
+// import Vuex from 'vuex'
 import baseStates from "./modules/baseState"
 import userInfor from "./modules/userInfo"
 
-Vue.use(Vuex)
+// Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
