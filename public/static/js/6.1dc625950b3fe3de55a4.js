adf4fe3a86baf6bd3d722353dfdd3a3feef3dfe8
cheng <1504468447@qq.com>
2019-01-11 17:07:07 +0800

    修改文本

--- public/static/js/6.1dc625950b3fe3de55a4.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([6],{"5D4o":function(t,e){},Fp9I:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var i={render:function(){var t=this.$createElement;return(this._self._c||t)("div",{staticStyle:{"text-align":"center","line-height":"300%","font-weight":"bold","font-size":"80px"}},[this._v("\n  coming soon ~~\n")])},staticRenderFns:[]};var o=n("C7Lr")({name:"for-ours",data:function(){return{}}},i,!1,function(t){n("5D4o")},"data-v-ca6231a2",null);e.default=o.exports}});
-//# sourceMappingURL=6.1dc625950b3fe3de55a4.js.map
