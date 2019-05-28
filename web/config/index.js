f9ff0ca7fc2c397f7ddd53eb74983f1ac34a26a1
Stur-Lion <1504468447@qq.com>
2019-05-28 15:03:01 +0800

    修改配置

--- web/config/index.js
+++ web/config/index.js
@@ -63,7 +63,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../public'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/wom/',
 
     /**
      * Source Maps
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,4 +1,5 @@
 export default new VueRouter({
+  base: '/wom/',
   mode: "history",
   routes: [
     {
