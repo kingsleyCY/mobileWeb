df7b584245f34eef069fd920e559cf18bcd7854b
cckings <1504468447@qq.com>
2019-07-11 23:17:37 +0800

    修改图片路径

--- web/config/prod.env.js
+++ web/config/prod.env.js
@@ -11,9 +11,9 @@ if (process.env.npm_lifecycle_event == 'cbuild') { /*dev*/
   BASE_URL = '"http://dev.lioncc.cn"'
   socket_URL = '"http://139.196.127.127:8807"'
 } else if (process.env.npm_lifecycle_event == 'build') { /*master*/
-  BASE_API = '"http://lionynn.cn"'
-  STATICS_API = '"http://lionynn.cn"'
-  BASE_URL = '"http://lionynn.cn"'
+  BASE_API = '"https://lionynn.cn"'
+  STATICS_API = '"https://lionynn.cn"'
+  BASE_URL = '"https://lionynn.cn"'
   socket_URL = '"http://119.3.77.140:8806"'
 }
 module.exports = {
